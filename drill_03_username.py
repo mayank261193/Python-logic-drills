ef619f21093c6d3cfcec81c815f0d732b5a2fde0
Mayank goyel <mayank261193@gmail.com>
2026-07-28 21:55:06 +0530

    modified the problem statement
    Refactored code to clean up full name and generate initials and username.

--- drill_03_username.py
+++ drill_03_username.py
@@ -3,7 +3,6 @@
 full_name = "  asha   verma  "
 birth_year = 1999
 
-# YOUR CODE HERE
 Clean=full_name.strip().title()
 Name=Clean.replace("","")
 Parts=Name.split()
@@ -12,6 +11,6 @@ print(Initials)
 Username=Parts[0]+str(birth_year)[-2:]
 print(Username)
 
-# Expected:
-# Initials: A.V.
-# Username: asha99
+
+ Initials: A.V.
+ Username: asha99
