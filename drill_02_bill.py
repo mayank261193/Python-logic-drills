80b8c323f143cfa6ceadb716088fb3eabea1f359
Mayank goyel <mayank261193@gmail.com>
2026-07-28 21:53:48 +0530

    Cleared spaces between elements

--- drill_02_bill.py
+++ drill_02_bill.py
@@ -1,6 +1,5 @@
 # Drill 2: splits a restaurant bill with a tip.
 
-# YOUR CODE HERE
 Total_bill=2847
 Tip=0.1*Total_bill
 Final_amount=Total_bill+Tip
@@ -9,7 +8,7 @@ N=4 #Number of person
 Am=Final_amount/N #Amount each person pays
 print(Am)
 print(round(Am))
-# Expected:
-# Total with tip: 3131.7
-# Each pays: 782.93
-# Each pays (whole rupees): 782
+
+Total with tip: 3131.7
+Each pays: 782.93
+Each pays (whole rupees): 782
