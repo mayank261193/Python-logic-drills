480e33ff8410aeec7b0282b1a590c82aa1786af0
Mayank goyel <mayank261193@gmail.com>
2026-08-10 21:03:53 +0530

    Delete drill_04_Remove_Duplicates.py

--- drill_04_Remove_Duplicates.py
+++
@@ -1,5 +0,0 @@
-List = ["Mayank", "Batman", "Mayank", "Ironman"]
-print("Initial_Count:",len(List))
-List = list(set(List))
-print("New_List:",List)
-print("Final_Count:",len(List)) #one item removed
