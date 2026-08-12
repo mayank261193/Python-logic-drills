6c17170e7fa8e3921e8fc08624182a9ca5c49ad1
Mayank goyel <mayank261193@gmail.com>
2026-08-12 11:44:34 +0530

    Change dictionary structure to include names and lengths

--- drill_07_Dictionary.py
+++ drill_07_Dictionary.py
@@ -1,5 +1,5 @@
 #Build a dictionary of word lengths from a sentence
 Sentence= "Mayank is a good boy"
 List=Sentence.split()
-Dictionary={s:len(s) for s in List}
-print(Dictionary)
+Dictionary=[{"Name":s, "Length":len(s)} for s in List]
+print(f"Dictionary:{Dictionary}")
